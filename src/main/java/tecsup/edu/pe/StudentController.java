ba506ab53cec79b0b10e58de69b285ad965c35e1
Joel Gutierrez <joelgp18@gmail.com>
2025-04-03 17:21:47 -0500

    Modificando la clase StudentController

--- src/main/java/tecsup/edu/pe/StudentController.java
+++ src/main/java/tecsup/edu/pe/StudentController.java
@@ -1,4 +1,13 @@
 package tecsup.edu.pe;
 
 public class StudentController {
+    private StudentService studentService = new StudentService();
+
+    public void addStudent(String studentName) {
+        studentService.addStudent(studentName);
+    }
+
+    public void printStudents() {
+        studentService.getStudents().forEach(System.out::println);
+    }
 }
