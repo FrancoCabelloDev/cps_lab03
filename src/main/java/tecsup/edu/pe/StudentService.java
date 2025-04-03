e066c806ca6a6c13df146e69fced6a92a5c62469
Joel Gutierrez <joelgp18@gmail.com>
2025-04-03 16:35:41 -0500

    modificando la clase StudentService

--- src/main/java/tecsup/edu/pe/StudentService.java
+++ src/main/java/tecsup/edu/pe/StudentService.java
@@ -1,4 +1,16 @@
 package tecsup.edu.pe;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudentService {
+    private List<String> students = new ArrayList<>();
+
+    public void addStudent(String studentName) {
+        students.add(studentName);
+    }
+
+    public List<String> getStudents() {
+        return students;
+    }
 }
