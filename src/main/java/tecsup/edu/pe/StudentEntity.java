4956a8b18afa25a354d9374bfef2d1c949442e62
Joel Gutierrez <joelgp18@gmail.com>
2025-04-03 17:21:47 -0500

    Modificando la clase StudentEntity

--- src/main/java/tecsup/edu/pe/StudentEntity.java
+++ src/main/java/tecsup/edu/pe/StudentEntity.java
@@ -1,4 +1,62 @@
 package tecsup.edu.pe;
 
 public class StudentEntity {
+
+    // Atributos
+    private int id;
+    private String name;
+    private int age;
+    private String email;
+
+    // Constructor
+    public StudentEntity(int id, String name, int age, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Método toString() para una representación fácil del objeto
+    @Override
+    public String toString() {
+        return "StudentEntity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
