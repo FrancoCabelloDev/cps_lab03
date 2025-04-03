c5a71928653fcac0a39b8e36eb30956e8b982f2a
Andersonbaltazar <franklin.baltazar@tecsup.edu.pe>
2025-04-03 17:19:57 -0500

    Modificando la clase student repository

--- src/main/java/tecsup/edu/pe/StudentRepository.java
+++ src/main/java/tecsup/edu/pe/StudentRepository.java
@@ -1,4 +1,18 @@
 package tecsup.edu.pe;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudentRepository {
+
+    private List<String> students = new ArrayList<>();
+
+    public void addStudent(String student) {
+        students.add(student);
+    }
+
+    public List<String> getAllStudents() {
+        return students;
+    }
 }
+
